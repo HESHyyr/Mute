037b38ea57b772cc83a72c14efa7ec40d371ae0f
HESHyyr <yurany@uci.edu>
2020-02-01 15:32:44 -0800

    Complete the minimum game loop

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -2,23 +2,63 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class UIManager : MonoBehaviour
 {
+    //Reference
     [SerializeField]
     private GameObject player;
+
+
     private GameObject healthBar;
+    private GameObject loseMenuPanel;
+    private GameObject winMenuPanel;
 
     // Start is called before the first frame update
     void Start()
     {
-        healthBar = transform.Find("Health Bar").gameObject;
+        Time.timeScale = 1;
+        if(SceneManager.GetActiveScene().name != "MainMenu")
+        {
+            healthBar = transform.Find("Health Bar").gameObject;
+            loseMenuPanel = transform.Find("Lose Button Group").gameObject;
+            winMenuPanel = transform.Find("Win Button Group").gameObject;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        healthBar.GetComponent<RectTransform>().sizeDelta = new Vector2(10, player.GetComponent<PlayerController>().playerHealth);
+        if (SceneManager.GetActiveScene().name != "MainMenu")
+            healthBar.GetComponent<RectTransform>().sizeDelta = new Vector2(10, player.GetComponent<PlayerController>().playerHealth);
     }
 
+    public void showGameOverMenu()
+    {
+        Time.timeScale = 0;
+        loseMenuPanel.SetActive(true);
+    }
+
+    public void showGameWinMenu()
+    {
+        Time.timeScale = 0;
+        winMenuPanel.SetActive(true);
+        
+    }
+
+    public void loadMainGame()
+    {
+        SceneManager.LoadScene("HeshScene");
+    }
+
+    public void loadMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void quit()
+    {
+        Application.Quit();
+    }
 }
